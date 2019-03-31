0e8c60309bc4defc2438b5e624f800314c1f6515
Pablo Kvitca <pablokvitca@gmail.com>
2019-03-31 02:22:43 -0400

    SQL for adding all colleges

---
+++ colleges.sql
@@ -0,0 +1,15 @@
+use projectcs3200;
+
+INSERT INTO 
+    college
+    (name)
+VALUES
+    ("Bouvé College of Health Sciences"),
+    ("College of Arts, Media and Design"),
+    ("Khoury College of Computer Sciences"),
+    ("College of Engineering"),
+    ("College of Professional Studies"),
+    ("College of Science"),
+    ("College of Social Sciences and Humanities"),
+    ("D'Amore-McKim School of Business"),
+    ("School of Law");
